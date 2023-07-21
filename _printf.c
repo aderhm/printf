f03e88b7419b558410c9fcd435f0e3eede0471a4
Abderrahim LAHYANI <lahyani.ad@gmail.com>
2023-07-21 11:24:59 +0100

    Initialize printf function

---
+++ _printf.c
@@ -0,0 +1,27 @@
+#include <unistd.h>
+#include <string.h>
+
+/**
+ * _printf - produces output according to a format.
+ * @format: a character string composed of zero or more directives.
+ *
+ * return: the number of characters printed.
+ */
+int _printf(const char *format, ...)
+{
+	write(1, format, strlen(format));
+	return (strlen(format));
+
+}
+
+/**
+ * main - check _printf.
+ *
+ * return: Always 0.
+ */
+int main(void)
+{
+	_printf("Hello, world");
+	write(1, "\n", 1);
+	return (0);
+}
